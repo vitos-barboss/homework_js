dbd3a06624b85ad21f70c2e94b273e595362f5fb
vitos-barboss <vitos-barboss@ukr.net>
2016-04-07 18:23:57 +0300

    2th commit

---
+++ homework_js1/hw1_task1-2/js/hw1_task1-2.js
@@ -0,0 +1,20 @@
+/**
+ * Created by barboss on 4/6/16.
+ */
+"use strict"
+
+var developer;
+var name = "Sergey";
+
+developer = name;
+
+console.log(developer);
+alert(developer);
+
+
+
+var userName = prompt("Enter your name", "");
+var askAgane = confirm("Is "+ userName + " your real name?");
+
+
+alert(askAgane);
